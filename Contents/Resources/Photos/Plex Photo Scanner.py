6b1c79aff55caa414b7695a7a154e61abb11d4d8
Elan Feingold <elan@bluemandrill.com>
2011-06-29 17:08:59 -1000

    Added year and creation time for photos.

--- Contents/Resources/Photos/Plex Photo Scanner.py
+++ Contents/Resources/Photos/Plex Photo Scanner.py
@@ -1,4 +1,4 @@
-import os
+import os, os.path, time
 import Filter, Media
 
 photo_exts = ['png','jpg','jpeg','bmp','gif','ico','tif','tiff','tga','pcx','dng','nef','cr2','crw','orf','arw','erf','3fr','dcr','x3f','mef','raf','mrw','pef','sr2']
@@ -14,5 +14,11 @@ def Scan(path, files, mediaList, subdirs, language=None, **kwargs):
     file = os.path.basename(path)
     title,ext = os.path.splitext(file)
     photo = Media.Photo(title)
+    
+    # Creation date, year.
+    created_at = time.localtime(os.path.getctime(path))
+    photo.released_at = time.strftime('%Y-%m-%d', created_at)
+    photo.year = int(time.strftime('%Y', created_at))
+    
     photo.parts.append(path)
     mediaList.append(photo)
