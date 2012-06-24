85d68458a3f8d2a8cb365b914cecf0af2d18007c
Elan Feingold <elan@plexapp.com>
2012-06-23 18:05:27 -1000

    Fixes for compilations and missing track indexes.

--- Contents/Resources/Music/Plex iTunes Scanner.py
+++ Contents/Resources/Music/Plex iTunes Scanner.py
@@ -1,4 +1,4 @@
-import urllib
+import urllib, re
 from xml.dom import minidom
 import Media, AudioFiles
 
@@ -6,42 +6,46 @@ Virtual = True
 
 def Scan(path, files, mediaList, subdirs, language=None):
   if len(path) == 0:
-    # Top level, artists.
-    dom = minidom.parse(urllib.urlopen('http://127.0.0.1:32400/music/iTunes/Artists'))
-    for artist in dom.getElementsByTagName('Artist'):
-      subdirs.append('/' + artist.getAttribute('key'))
+    # Top level, albums.
+    dom = minidom.parse(urllib.urlopen('http://127.0.0.1:32400/music/iTunes/Albums'))
+    for album in dom.getElementsByTagName('Album'):
+      subdirs.append('/' + album.getAttribute('key'))
   else:
+    # Tracks.
     paths = path.split('/')
-    if len(paths) == 1:
-      # Albums for artist.
-      dom = minidom.parse(urllib.urlopen('http://127.0.0.1:32400/music/iTunes/Artists/%s' % paths[0]))
-      for album in dom.getElementsByTagName('Album'):
-        subdirs.append('/' + path + '/' + album.getAttribute('key'))
-    elif len(paths) == 2:
-      # Tracks, let's finally add it.
-      dom = minidom.parse(urllib.urlopen('http://127.0.0.1:32400/music/iTunes/Artists/%s/%s' % (paths[0], paths[1])))
-      artist_album_map = {}
-      compilation_count = 0
-      for track in dom.getElementsByTagName('Track'):
-        # Figure out album artist.
-        album_artist = track.getAttribute('albumArtist').strip()
-        artist_album_map[album_artist] = True
-        if len(album_artist) == 0: album_artist = None
-        else: album_artist = album_artist.encode('utf-8')
-        
-        # Add the track.
-        t = Media.Track(artist = track.getAttribute('artist').encode('utf-8'),
-                        album = track.getAttribute('album').encode('utf-8'),
-                        title = track.getAttribute('track').encode('utf-8'),
-                        index = int(track.getAttribute('index')),
-                        album_artist = album_artist,
-                        disc = int(track.getAttribute('disc')))
-        if track.getAttribute('compilation') == '1': compilation_count = compilation_count + 1
-        t.parts.append(track.getAttribute('file'))
-        mediaList.append(t)
+    dom = minidom.parse(urllib.urlopen('http://127.0.0.1:32400/music/iTunes/Albums/%s' % paths[0]))
+    artist_album_map = {}
+    compilation_count = 0
+    for track in dom.getElementsByTagName('Track'):
+      # Figure out album artist.
+      album_artist = track.getAttribute('albumArtist').strip()
+      artist_album_map[album_artist] = True
+      if len(album_artist) == 0: album_artist = None
+      else: album_artist = album_artist.encode('utf-8')
       
-      # If we're listed as a compilation, make sure all album artists are there and the same.
-      if compilation_count > 0 and (len(artist_album_map) > 1 or len(artist_album_map.keys()[0])):
-        for t in mediaList:
-          t.album_artist = 'Various Artists'
-        
+      # Track index, do a bit of extra work.
+      index = int(track.getAttribute('index'))
+      file = track.getAttribute('file').split('/')[-1]
+      print re.findall('[.\-]+[ ]*([0-9]{2})[ ]*[.\-]', file)
+      print re.findall('^([0-9]{2})[ .\-]', file)
+      if index == 0:
+        try: index = int(re.findall('[.\-]+[ ]*([0-9]{2})[ ]*[.\-]', file)[0])
+        except: 
+          try: index = int(re.findall('^([0-9]{2})[ .\-]', file)[0])
+          except: pass
+      
+      # Add the track.
+      t = Media.Track(artist = track.getAttribute('artist').encode('utf-8'),
+                      album = track.getAttribute('album').encode('utf-8'),
+                      title = track.getAttribute('track').encode('utf-8'),
+                      index = index,
+                      album_artist = album_artist,
+                      disc = int(track.getAttribute('disc')))
+      if track.getAttribute('compilation') == '1': compilation_count = compilation_count + 1
+      t.parts.append(track.getAttribute('file'))
+      mediaList.append(t)
+    
+    # If we're listed as a compilation, make sure all album artists are there and the same.
+    if compilation_count > 0 and (len(artist_album_map) > 1 or len(artist_album_map.keys()[0]) == 0):
+      for t in mediaList:
+        t.album_artist = 'Various Artists'
